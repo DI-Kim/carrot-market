49c86dc4ef06e27c6c1720893ac78025d71fd41c
bigperson <daein7076@gmail.com>
2023-11-14 09:24:27 +0900

    4.5 transitions

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
           <span>Total</span>
           <span className="font-semiblod">$99</span>
         </div>
-        <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
+        <button className="block mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
           Checkout
         </button>
       </div>
@@ -45,7 +45,7 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      <div className="bg-white p-10 rounded-2xl shadow-xl">
+      <div className="bg-white p-6 rounded-3xl shadow-xl">
         <div className="flex justify-between items-center mb-5">
           <span>⬅️</span>
           <div className="space-x-3">
@@ -59,9 +59,9 @@ const Home: NextPage = () => {
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
             <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-500"></button>
-              <button className="w-5 h-5 rounded-full bg-indigo-500"></button>
-              <button className="w-5 h-5 rounded-full bg-teal-500"></button>
+              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition"></button>
+              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition"></button>
+              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition"></button>
             </div>
             <div className="flex items-center space-x-5">
               <button className="bg-blue-200 flex justify-center items-center aspect-square w-8 font-medium text-xl text-gray-500 rounded-lg">
