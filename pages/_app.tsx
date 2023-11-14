95dba52f9a6c7ffdcb07812bc1c3d7a3ea6349d2
bigperson <daein7076@gmail.com>
2023-11-14 16:47:50 +0900

    start Make UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,5 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <button
-        onClick={() => {
-          const component = document.getElementById("mode");
-          if (component?.classList.contains("dark")) component?.classList.remove("dark");
-          else component?.classList.add("dark");
-        }}
-      >
-        dark | light
-      </button>
-      <div className="" id="mode">
-        <Component {...pageProps} />
-      </div>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
