a6dfca0dec341afe6453c31c641de49e10811d97
bigperson <daein7076@gmail.com>
2023-11-16 19:53:10 +0900

    7.4 Validation part.2

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,27 +1,48 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface IForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onValid = () => {
-    console.log("i'm valid");
+  const { register, handleSubmit } = useForm<IForm>();
+
+  const onValid = (data: IForm) => {
+    console.log("good");
   };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form
+      onSubmit={handleSubmit(onValid, onInvalid)}
+      className="flex flex-col space-y-10"
+    >
       <input
-        {...register("username", { required: true })}
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            message: "username should be longer than 5 chars",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="Username"
       />
       <input
-        {...(register("email"), { required: true })}
+        {...register("email", { required: "Email is required" })}
         type="email"
         placeholder="Email"
       />
       <input
-        {...(register("password"), { required: true })}
+        {...register("password", { required: "Password is required" })}
         type="password"
         placeholder="Password"
       />
+
       <input type="submit" value="Create Account" />
     </form>
   );
