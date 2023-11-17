893e9aa6454a7ddd172d4c31381bb0d93e5f51c5
bigperson <daein7076@gmail.com>
2023-11-17 16:58:51 +0900

    9.1 Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,15 +3,61 @@ import withHandler from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+
+  const payload = phone ? { phone: +phone } : { email };
+  //upsert
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email: email,
+  //     },
+  //   });
+  //   if (user) console.log("Find User", user);
+  //   if (!user) {
+  //     console.log("Did not find. Will create");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         email: email,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (user) console.log("Find User", user);
+  //   if (!user) {
+  //     console.log("Did not find. Will create");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+
   return res.json({ ok: true });
 }
 
 export default withHandler("POST", handler);
-/**
- * login ways
- * phone number -> user is exist?
- * --> token --> user #123456
- * --> #123456 --> SMS --> phon number (twilio)
- * --> #123456 --> token is exist? --> user --> login
- */
