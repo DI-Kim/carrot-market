2b0cca5914926fcc435610c85524953c4aa31287
bigperson <daein7076@gmail.com>
2023-11-17 10:42:40 +0900

    8.0 Enter Form

--- components/input.tsx
+++ components/input.tsx
@@ -1,16 +1,24 @@
+import type { UseFormRegisterReturn } from "react-hook-form";
+
 interface InputProps {
   label: string;
   name: string;
   kind?: "text" | "phone" | "price";
-  [key: string]: any;
+  register: UseFormRegisterReturn;
+  type: string;
+  required?: boolean;
+  // [key: string]: any;
 }
 
 export default function Input({
   label,
   name,
   kind = "text",
-  ...rest
-}: InputProps) {
+  register,
+  required,
+  type,
+} // ...rest
+: InputProps) {
   return (
     <div>
       <label
@@ -23,7 +31,10 @@ export default function Input({
         <div className="relative flex items-center  rounded-md shadow-sm">
           <input
             id={name}
-            {...rest}
+            {...register}
+            type={type}
+            required={required}
+            // {...rest}
             className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
           />
         </div>
@@ -35,7 +46,10 @@ export default function Input({
           </div>
           <input
             id={name}
-            {...rest}
+            {...register}
+            type={type}
+            required={required}
+            // {...rest}
             className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 pl-7 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
           />
           <div className="pointer-events-none absolute right-0 flex items-center pr-3">
@@ -50,7 +64,10 @@ export default function Input({
           </span>
           <input
             id={name}
-            {...rest}
+            {...register}
+            type={type}
+            required={required}
+            // {...rest}
             className="w-full appearance-none rounded-md rounded-l-none border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
           />
         </div>
