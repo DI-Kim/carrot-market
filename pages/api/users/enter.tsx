ef1c8dcaa6809e7d42a96bd3b5aeb79193f9fbc1
bigperson <daein7076@gmail.com>
2023-11-17 18:17:16 +0900

    9.4 Sending SMS

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,10 @@
+import twilio from "twilio";
 import client from "@/libs/server/client";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import { Twilio } from "twilio";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -30,6 +34,16 @@ async function handler(
     },
   });
   console.log(token);
+
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      // to: phone
+      to: process.env.TWILIO_PHONE!,
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message);
+  }
 
   return res.json({
     ok: true,
