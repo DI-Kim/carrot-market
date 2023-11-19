5126d9f3cebfaf9d4f55f1a3633f5b94e0f75084
bigperson <daein7076@gmail.com>
2023-11-19 18:52:56 +0900

    9.7 Serverless Sesstions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,14 +1,32 @@
 import client from "@/libs/server/client";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import { withIronSessionApiRoute } from "iron-session/next";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>,
-) {
+): Promise<any> {
+  console.log(req.session);
+
   const { token } = req.body;
-  console.log(token);
+
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    // include: { user: true },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
+
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "923184723947231981236847621398463278944723918",
+});
