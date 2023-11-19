6875a39b9165b7a1f5078c1b84b63e149221001b
bigperson <daein7076@gmail.com>
2023-11-19 21:17:25 +0900

    10.3 SWR | AWESOME!!!

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,23 +1,17 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
-  const [user, setUser] = useState();
+  const { data, error } = useSWR("/api/users/me", fetcher);
 
   const router = useRouter();
 
-  useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.ok) {
-          //! replace: 브라우저에 히스토리를 남기지 않음. 따라서 권한이 없으면 replace를 사용하여 히스토리를 남기지 말자.
-          //! push: 성공적으로 로딩됐다면 브라우저에 히스토리를 남김.
-          return router.replace("/enter");
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
+  //! replace: 브라우저에 히스토리를 남기지 않음. 따라서 권한이 없으면 replace를 사용하여 히스토리를 남기지 말자.
+  //! push: 성공적으로 로딩됐다면 브라우저에 히스토리를 남김.
+  // return router.replace("/enter");
 
-  return user;
+  return data;
 }
