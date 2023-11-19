303d433208cfce65dfdaacf4d2463db07779cca5
bigperson <daein7076@gmail.com>
2023-11-19 21:27:41 +0900

    10.4 useUser Refactor

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -2,16 +2,17 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then((response) => response.json());
-
 export default function useUser() {
-  const { data, error } = useSWR("/api/users/me", fetcher);
-
+  const { data, error } = useSWR("/api/users/me");
   const router = useRouter();
 
-  //! replace: 브라우저에 히스토리를 남기지 않음. 따라서 권한이 없으면 replace를 사용하여 히스토리를 남기지 말자.
-  //! push: 성공적으로 로딩됐다면 브라우저에 히스토리를 남김.
-  // return router.replace("/enter");
+  useEffect(() => {
+    if (data && !data.ok) {
+      //! replace: 브라우저에 히스토리를 남기지 않음. 따라서 권한이 없으면 replace를 사용하여 히스토리를 남기지 말자.
+      //! push: 성공적으로 로딩됐다면 브라우저에 히스토리를 남김.
+      router.replace("/enter");
+    }
+  }, [data, router]);
 
-  return data;
+  return { user: data?.profile, isLoading: !data && !error };
 }
